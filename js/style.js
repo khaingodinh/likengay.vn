c5e35ce07d26b0d32f3ea97503d47c2bd4e85af7
khaingodinh <ndk1812@gmail.com>
2021-05-02 18:42:37 +0700

    update source

--- js/style.js
+++ js/style.js
@@ -20,3 +20,37 @@ $('#amount_filter').click(function() {
 $('#amount_filter_select ul li').click(function() {
     $('#amount_filter .amount-filter__text').html($(this).html());
 });
+
+$('#menu-sp').click(function() {
+    $('.content-wrapper').hide();
+    $('.right-menu').removeAttr('style');
+    $('.left-menu')
+        .css('width', '100%')
+        .css('background-color', '#fff')
+        .css('padding-left', '20px')
+        .css('display', 'block');
+});
+
+$('.j-currency').click(function() {
+    $('.content').hide();
+    $('.left-menu').removeAttr('style');
+    $('.right-menu')
+        .css('width', '100%')
+        .css('padding', '15px')
+        .css('margin', '0')
+        .css('background-color', '#fff')
+        .css('display', 'block');
+});
+
+$('.left-menu .sub-menu-item a').click(function() {
+    $('.left-menu').removeAttr('style');
+    $('.content-wrapper').show();
+    $('.content').show();
+    var menu = $(this).data('menu');
+    $('#container').load("components/" + menu + ".html", function(responseTxt, statusTxt, xhr){
+        // if(statusTxt == "success")
+        //   alert("External content loaded successfully!");
+        // if(statusTxt == "error")
+        //   alert("Error: " + xhr.status + ": " + xhr.statusText);
+    });
+});
