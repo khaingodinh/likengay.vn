5d8f404fae12d5c34e6047edc8f05edca47afb0f
DESKTOP-Q9QK1SG\khaingodinh <khai.ngo@2clicksolutions.com>
2021-05-06 15:41:06 +0700

    apply proccessing like fanpage

--- js/pages/like-fanpage.js
+++ js/pages/like-fanpage.js
@@ -1,3 +1,21 @@
 $('#history-btn').click(function() {
     $('#container').load("components/transaction-history.html");
 });
+
+$('.like-fanpage__form__btn').click(function() {
+    $(this).hide();
+    $('.like-fanpage__form__processing').show();
+    window.moveProgressBar(function () {
+        // $('.like-fanpage__form__processing').html(
+        //     '<div class="d-flex align-items-center justify-content-center flex-column w-100">' + 
+        //         '<img src="images/process-success.png" alt="">' +
+        //         '<span>Bạn đã mua thành công 500 like với giá 60đ</span>' +
+        //     '</div>');
+        $('.like-fanpage__form__processing').html(
+            '<div class="d-flex align-items-center justify-content-center flex-column w-100">' + 
+                '<img src="images/process-fail.png" alt="">' +
+                '<span>Tạo tiến trình thất bại</span>' +
+                '<span>Tài khoản của bạn không đủ tiền</span>' +
+            '</div>');
+    });
+});
